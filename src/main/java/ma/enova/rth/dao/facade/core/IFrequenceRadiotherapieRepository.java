fbc66a777eae5ec9661efe9d6b9c53fbc291c00a
younes.zouani <y.zouani@uca.ma>
2022-12-20 17:29:31 +0100

    fix specification of organe

--- src/main/java/ma/enova/rth/dao/facade/core/IFrequenceRadiotherapieRepository.java
+++ src/main/java/ma/enova/rth/dao/facade/core/IFrequenceRadiotherapieRepository.java
@@ -1,18 +1,17 @@
 package ma.enova.rth.dao.facade.core;
 
 import ma.enova.rth.domain.core.FrequenceRadiotherapie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
  * Interface du DAO IfrequenceRadiotherapie
+ *
  * @author JAF
  * @version 1.2
  */
- 
-@Repository 
-public interface IFrequenceRadiotherapieRepository extends JpaRepository<FrequenceRadiotherapie	, Long> ,JpaSpecificationExecutor<FrequenceRadiotherapie> {
+
+@Repository
+public interface IFrequenceRadiotherapieRepository extends AbstractRepository<FrequenceRadiotherapie, Long> {
 
 
 }
